723a612c744395d25fe3aa8e05038da4d7891594
Dylan Thomson <djthomson88@gmail.com>
2017-07-03 12:07:31 -0400

    Comments and cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
+	// Form submit, get search value and make request
 	$("form:first").submit(function(event) {
 		event.preventDefault();
+
+		// Get search value from input field then clear it
 		var searchTerm = $("input:first").val();
 		$("input:first").val("");
+
+		// Make API request using search value
 		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchTerm + "&callback=?", function(data) {
 			console.log(data.query.search);
+
+			// Update/clear output
 			$(".searchKeyOutput").html("").html(data.query.search.length + " results for: " + searchTerm);
 			$(".searchOutput").html("");
+
+			// Display each article
 			data.query.search.forEach(function(article) {
 				var title = article.title;
 				var url = "https://en.wikipedia.org/wiki/" + title;
