714efbbef025db2c0e746296d6a0ff9ad45ed3cf
Dylan Thomson <djthomson88@gmail.com>
2017-07-03 11:32:30 -0400

    Display search term submitted
    -Display the submitted search term and the number of results returned

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,16 @@ $(document).ready(function() {
 	$("form:first").submit(function(event) {
 		event.preventDefault();
 		var searchTerm = $("input:first").val();
-		console.log("searched for:", searchTerm);
 		$("input:first").val("");
 		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchTerm + "&callback=?", function(data) {
 			console.log(data.query.search);
+			$(".searchKeyOutput").html("").html(data.query.search.length + " results for: " + searchTerm);
 			$(".searchOutput").html("");
 			data.query.search.forEach(function(article) {
 				var title = article.title;
 				var url = "https://en.wikipedia.org/wiki/" + title;
 				$(".searchOutput").append("<div><a href='" + url + "' target='_blank'>" + title + "</a></div>");	
 			});
-		});
-		
+		});	
 	});
 });
