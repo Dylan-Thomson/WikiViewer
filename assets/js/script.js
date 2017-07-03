41709e9a7d11491bfde6492279d815938e23080b
Dylan Thomson <djthomson88@gmail.com>
2017-07-03 10:44:51 -0400

    Get search term, clear previous output
    -Make request using value in text input
    -Clear previous searches before populating page with new ones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 	$(".btn").on("click", function() {
-		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=egg&callback=?", function(data) {
+		var searchTerm = $("input").val();
+		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchTerm + "&callback=?", function(data) {
 			console.log(data.query.search);
+			$(".searchOutput").html("");
 			data.query.search.forEach(function(article) {
 				var title = article.title;
 				var url = "https://en.wikipedia.org/wiki/" + title;
-				$(".col-xs-12").append("<div><a href='" + url + "' target='_blank'>" + title + "</a></div>");	
+				$(".searchOutput").append("<div><a href='" + url + "' target='_blank'>" + title + "</a></div>");	
 			});
 		});
 	});
